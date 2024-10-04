e730364ab44b2ae8f7ac45c0888ac94fb676d4b0
ASHUTOSH-13 <ashutoshkalash03@gmail.com>
2024-10-04 16:46:20 +0530

    new files have been added, learned about dotenv and mongodb atlas

--- mongodb.js
+++ mongodb.js
@@ -6,8 +6,9 @@
 const mongoose = require("mongoose");
 
 // define the mongoDB url
-const mongodbURL = "mongodb://localhost:27017/mydatabase"; // mydatabase is the name of the database that we want to create
-
+//const mongodbURL = "mongodb://localhost:27017/mydatabase"; // mydatabase is the name of the database that we want to create
+const mongodbURL =
+  "mongodb://ashutoshkalash03:123Ashu890@cluster0-shard-00-00.no0om.mongodb.net:27017,myDatabase?retryWrites=true&w=majority";
 // establishing connection..
 
 mongoose.connect(mongodbURL, {
@@ -32,10 +33,13 @@ db.on("disconnected", () => {
   console.log("the database is disconnected");
 });
 
-//export the database connection 
+//export the database connection
 
-module.exports = db ;
+module.exports = db;
 
+// model is the blueprint of the database
+// once you created the database you can do the CRUD operation
 
-// model is the blueprint of the database 
-// once you created the database you can do the CRUD operation 
+// dotenv is a module in nodejs that is used to manage configuration variable and sensitive information in your application ,
+// it is particularly useful in keeping sensitive data like apis, database connection strings and other environment specific configurations seperate from your code
+// npm install dotenv
